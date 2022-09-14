8476afab337d30e288a68acc897b23546e7e80e3
kelvin <kelvinperrie@gmail.com>
2022-09-14 13:07:27 +1200

    Initial commit

---
+++ scripts/01puzzle.js
@@ -0,0 +1,6 @@
+
+$(document).ready(function() {
+    $(".mysterous-area").on("click", function() {
+        window.location.href = "02puzzle-wow.html"; // lol if you just look at this source, nice!
+    })
+});
---
+++ scripts/03puzzle-yep.js
@@ -0,0 +1,2 @@
+
+// not much javascript going on here. Go to 04puzzle-hmm.html
